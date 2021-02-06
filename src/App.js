2cd87bc7d253ea7e0fdc93595f91048504f1c905
Yesayan <yesayanyesayan23@gmail.com>
2021-02-06 23:22:50 +0400

    Commit_Message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Counter from './Components/Counter';
+import ToDo from './Components/ToDo';
 
 function App() {
   
   return (
     <div className="App">
-      <Counter counter={2} /> 
+    <ToDo />
     </div>
   );
 }
